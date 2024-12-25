483fe18f26c25f285a4e830221b1a4a7d8c44c9d
seunghoon <zlzlzlmo@daum.net>
2024-12-25 19:18:26 +0900

    tsconfig 설정

---
+++ src/lotto.types.ts
@@ -0,0 +1,25 @@
+interface PrizeTier {
+  rank: number;
+  winners: number;
+  prize: number;
+}
+
+interface PrizeStatistics {
+  totalPrize: number;
+  winners: number;
+  tiers: PrizeTier[];
+}
+
+interface LottoDraw {
+  id: number;
+  date: string;
+  winningNumbers: number[];
+  bonusNumber: number;
+  prizeStatistics: PrizeStatistics;
+}
+
+export interface FindAllResponse {
+  status: "success" | "error";
+  data: LottoDraw[];
+  message?: string;
+}
